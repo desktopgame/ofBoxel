39488e5d3adb06df911860b61fecfca85f2af4fc
koya <gnndr36@gmail.com>
2022-05-01 22:24:38 +0900

    Update: それっぽいものを表示できるように

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,13 +6,47 @@ void ofApp::setup() {
   assert(m_shader.load("boxel.vert", "boxel.frag"));
   // 画像読み込み
   ofDisableArbTex();
-  assert(m_image.load("debug.png"));
+  assert(m_image.load("textureMap.png"));
   // ワールドの生成
   ofMesh mesh = ofMesh::plane(1.0f, 1.0f, 2, 2, OF_PRIMITIVE_TRIANGLES);
   this->m_boxelRenderer =
       std::make_unique<ofBoxel::BoxelRenderer>(m_shader, mesh);
-  for (int i = 0; i < 6; i++) {
-    m_boxelRenderer->batch(glm::vec3(0, 0, 0), i, i, i);
+  // 地面
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 6; j++) {
+      m_boxelRenderer->batch(glm::vec3(0, 0, -i), 2, 2, 1);
+    }
+    for (int j = 0; j < 6; j++) {
+      m_boxelRenderer->batch(glm::vec3(i, 0, -9), 5, 5, 1);
+    }
+    for (int j = 0; j < 6; j++) {
+      m_boxelRenderer->batch(glm::vec3(i, 0, 0), 4, 4, 1);
+    }
+    for (int j = 0; j < 6; j++) {
+      m_boxelRenderer->batch(glm::vec3(9, 0, -i), 3, 3, 1);
+    }
+    for (int j = 0; j < 10; j++) {
+      for (int k = 0; k < 6; k++) {
+        m_boxelRenderer->batch(glm::vec3(j, 0, -i), 0, 0, 2);
+      }
+    }
+  }
+  // 木
+  for (int i = 0; i < 4; i++) {
+    // 上と下はいらないですが、サンプルなので手抜き
+    for (int k = 0; k < 6; k++) {
+      m_boxelRenderer->batch(glm::vec3(4, 1 + i, -4), k, k, 8);
+    }
+  }
+  // 葉っぱ
+  for (int x = 3; x < 6; x++) {
+    for (int z = -3; z > -6; z--) {
+      for (int y = 0; y < 3; y++) {
+        for (int t = 0; t < 6; t++) {
+          m_boxelRenderer->batch(glm::vec3(x, 5 + y, z), t, t, 16);
+        }
+      }
+    }
   }
 }
 
